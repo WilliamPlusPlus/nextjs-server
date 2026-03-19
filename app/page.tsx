def29a533e4668b2a282b4ff321cd9a50367a6ad
William Phenicie <william@phenicie.me>
2026-03-19 03:34:51 -0500

    Continuity

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
 	return (
 	  <>
 		<h1 className="text-4xl font-bold underline">Hello!</h1>
-	        <p className="font-mono">This HTML is in a separate file</p>
+	        <p className="font-mono">This HTML is no longer in a separate file</p>
 	  </>
 	)
 
