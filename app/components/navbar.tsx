c785e8735edc0195eb939faf6d5e84d052df4b94
William Phenicie <william@phenicie.me>
2026-03-19 03:07:02 -0500

    Fixing the error

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <nav style={{
       display: "flex",
+      justifyContent: "center",
       gap: "20px",
       padding: "20px",
       borderBottom: "1px solid #333",
       color: "white",
-      min-height: "100vh",
     }}>
       <Link href="/">Home</Link>
       <Link href="/time">Server Time</Link>
